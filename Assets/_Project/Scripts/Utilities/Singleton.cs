4e803d35407b5c1fdc352743e32f349eb49ebfb4
Aayush Kashyap <33807516+TheKing0x9@users.noreply.github.com>
2023-02-04 12:38:21 +0530

    Add utility scripts (#1)
    * Added utility scripts

---
+++ Assets/_Project/Scripts/Utilities/Singleton.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+namespace GGJ.Utilities {
+    public class Singleton<T> : MonoBehaviour where T : Singleton<T> {
+        protected static T _instance;
+
+        public static T instance {
+            get {
+                if (_instance != null) return _instance;
+
+                _instance = GameObject.FindObjectOfType<T> ();
+
+                if(_instance != null) return _instance;
+
+                GameObject singletonObj = new GameObject();
+                singletonObj.name = typeof(T).ToString();
+                _instance = singletonObj.AddComponent<T>();
+
+                return _instance;
+            }
+        }
+
+        protected virtual void Awake() {
+            if (_instance != null) {
+                Destroy(gameObject);
+                return;
+            }
+
+            _instance = (T)this;
+        }
+
+        protected virtual void OnDestroy() {
+            if (_instance == this) _instance = null;
+        }
+    }
+}
