18cb7bb07dc9525fae1d32e78370b9d342ce2b10
Aaysuh Kashyap <kashyapaayush32@gmail.com>
2023-02-04 12:31:05 +0530

    Added utility scripts

---
+++ Assets/_Project/Scripts/Utilities/PersistentSingleton.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Platformer.Utilities {
+    public class PersistentSingleton<T> : Singleton<T> where T : Singleton<T> {
+        protected override void Awake() {
+            base.Awake();
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
